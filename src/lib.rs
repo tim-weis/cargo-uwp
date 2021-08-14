b8ed2ad384a59ba40e50c8ef4b9397b20eb1c811
Tim Weis <tim.weis@outlook.com>
2021-08-14 17:13:51 +0200

    Improved OOB experience

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub const PACKAGE_IDENTITY_NAME_KEY: &str = "package-identity-name";
+pub const PACKAGE_IDENTITY_NAME_DEFAULT: &str = "PackageIdentityNameDefault";
+
+pub const PACKAGE_IDENTITY_PUBLISHER_KEY: &str = "package-identity-publisher";
+pub const PACKAGE_IDENTITY_PUBLISHER_DEFAULT: &str = "CN=00000000-0000-0000-0000-000000000000";
+
+pub const PACKAGE_PUBLISHER_DISPLAY_NAME_KEY: &str = "package-properties-publisherdisplayname";
+pub const PACKAGE_PUBLISHER_DISPLAY_NAME_DEFAULT: &str = "PublisherDisplayNameDefault";
+
+pub const PACKAGE_VISUAL_DISPLAY_NAME_KEY: &str = "package-applications-visualelements-displayname";
+pub const PACKAGE_VISUAL_DISPLAY_NAME_DEFAULT: &str = "My Rust UWP application";
+
+pub const PACKAGE_VISUAL_DESCRIPTION_KEY: &str = "package-applications-visualelements-description";
+pub const PACKAGE_VISUAL_DESCRIPTION_DEFAULT: &str = "A UWP application written in Rust";
